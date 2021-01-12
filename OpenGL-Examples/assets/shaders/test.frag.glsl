56f8ac4462522bab67ba58db0cb9671586e4d4d5
DESKTOP-68QQH5S\Seongho <nsho77@naver.com>
2021-01-12 22:20:48 +0900

    Texture batch rendering

--- OpenGL-Examples/assets/shaders/test.frag.glsl
+++ OpenGL-Examples/assets/shaders/test.frag.glsl
@@ -2,9 +2,14 @@
 
 layout (location = 0) out vec4 o_Color;
 
-uniform vec4 u_Color;
+in vec4 v_Color;
+in vec2 v_TexCoord;
+in float v_TexIndex;
+
+uniform sampler2D u_Textures[2];
 
 void main()
 {
-	o_Color = u_Color;
+	int index = int(v_TexIndex);
+	o_Color = texture(u_Textures[index], v_TexCoord);
 }
--- OpenGL-Examples/src/ExampleLayer.cpp
+++ OpenGL-Examples/src/ExampleLayer.cpp
@@ -1,5 +1,7 @@
 #include "ExampleLayer.h"
 
+#include "stb_image/stb_image.h"
+
 using namespace GLCore;
 using namespace GLCore::Utils;
 
@@ -12,6 +14,26 @@ ExampleLayer::ExampleLayer()
 ExampleLayer::~ExampleLayer()
 {
 
+}
+
+static GLuint LoadTexture(const std::string& path)
+{
+	int w, h, bits;
+
+	stbi_set_flip_vertically_on_load(1);
+	auto* pixels = stbi_load(path.c_str(), &w, &h, &bits, STBI_rgb);
+	GLuint textureID;
+	glCreateTextures(GL_TEXTURE_2D, 1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+
+	stbi_image_free(pixels);
+
+	return textureID;
 }
 
 void ExampleLayer::OnAttach()
@@ -27,27 +49,41 @@ void ExampleLayer::OnAttach()
 		"assets/shaders/test.frag.glsl"
 	);
 
+	glUseProgram(m_Shader->GetRendererID());
+	auto loc = glGetUniformLocation(m_Shader->GetRendererID(), "u_Textures");
+	int samplers[2] = { 0, 1 };
+	glUniform1iv(loc, 2, samplers);
+
 	glCreateVertexArrays(1, &m_QuadVA);
 	glBindVertexArray(m_QuadVA);
 
 	float vertices[] = {
-		-1.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f,
-		-1.5f,  0.5f, 0.0f,
+		-1.5f, -0.5f, 0.0f, 0.18f, 0.6f, 0.96f, 1.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.18f, 0.6f, 0.96f, 1.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f, 0.0f, 0.18f, 0.6f, 0.96f, 1.0f, 1.0f, 1.0f, 0.0f,
+		-1.5f,  0.5f, 0.0f, 0.18f, 0.6f, 0.96f, 1.0f, 0.0f, 1.0f, 0.0f,
 
-		 0.5f, -0.5f, 0.0f,
-		 1.5f, -0.5f, 0.0f,
-		 1.5f,  0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f, 0.0f, 0.0f, 1.0f,
+		 1.5f, -0.5f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f, 1.0f, 0.0f, 1.0f,
+		 1.5f,  0.5f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f, 1.0f, 1.0f, 1.0f,
+		 0.5f,  0.5f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f, 0.0f, 1.0f, 1.0f
 	};
 
 	glCreateBuffers(1, &m_QuadVB);
 	glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+	glEnableVertexArrayAttrib(m_QuadVB, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 10 * sizeof(float), 0);
+
+	glEnableVertexArrayAttrib(m_QuadVB, 1);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 10 * sizeof(float), (const void*)12);
+
+	glEnableVertexArrayAttrib(m_QuadVB, 2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 10 * sizeof(float), (const void*)28);
+
+	glEnableVertexArrayAttrib(m_QuadVB, 3);
+	glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, 10 * sizeof(float), (const void*)36);
 
 	uint32_t indices[] = { 
 		0, 1, 2, 2, 3, 0,
@@ -56,6 +92,9 @@ void ExampleLayer::OnAttach()
 	glCreateBuffers(1, &m_QuadIB);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_QuadIB);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	m_ChernoTex = LoadTexture("assets/textures/ChernoLogo.png");
+	m_CheckerboardTex = LoadTexture("assets/textures/Checkerboard.png");
 }
 
 void ExampleLayer::OnDetach()
@@ -92,12 +131,14 @@ void ExampleLayer::OnUpdate(Timestep ts)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glUseProgram(m_Shader->GetRendererID());
+	glBindTextureUnit(0, m_ChernoTex);
+	glBindTextureUnit(1, m_CheckerboardTex);
 
 	int location = glGetUniformLocation(m_Shader->GetRendererID(), "u_ViewProjection");
 	glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(m_CameraController.GetCamera().GetViewProjectionMatrix()));
 
-	location = glGetUniformLocation(m_Shader->GetRendererID(), "u_Color");
-	glUniform4fv(location, 1, glm::value_ptr(m_SquareColor));
+	//location = glGetUniformLocation(m_Shader->GetRendererID(), "u_Color");
+	//glUniform4fv(location, 1, glm::value_ptr(m_SquareColor));
 
 	glBindVertexArray(m_QuadVA);
 	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
